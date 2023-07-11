975cfba6cc251ce3c42fa228a241da91b7fee7fc
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-11 13:01:24 +0200

    Create some queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
-
+/* 1-- Create table animals */
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
@@ -8,3 +8,85 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+----------------------------------------------------------------
+/* 2-- Update and delete table */
+----------------------------------------------------------------
+
+/*- Update the animals table by setting the species column to unspecified*/
+BEGIN;
+UPDATE animals SET species = 'undefined';
+
+/*- Verify change*/
+
+ SELECT * FROM animals;
+
+ /* Roll back the change and verify that the species columns went back to the state before the transaction.*/
+
+ROLLBACK;
+SELECT * FROM animals;
+----------------------------------------------------------------
+/* Update the animals table by setting the species column to digimon for all animals that have a name ending in mon. */
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+/* Update the animals table by setting the species column to pokemon for all animals that don't have species already set. */
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+/* Verify that changes were made. */
+SELECT * FROM animals;
+
+/* Commit the transations */
+COMMIT;
+
+/* Verify that changes are there after commit. */
+SELECT * FROM animals;
+----------------------------------------------------------------
+/* Inside a transaction delete all records in the animals table, then roll back the transaction. */
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+----------------------------------------------------------------
+
+/* Inside a transaction*/
+
+/* Delete all animals born after Jan 1st, 2022.*/
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+/* Create a savepoint for the transaction.*/
+SAVEPOINT savepoint_before_update_animals_weight;
+
+
+/* Update all animals' weight to be their weight multiplied by -1.*/
+UPDATE animals SET weight_kg = weight_kg *-1;
+
+/* Rollback to the savepoint */
+ROLLBACK TO savepoint_before_update_animals_weight;
+
+/* Update all animals' weights that are negative to be their weight multiplied by -1.*/
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+/*Commit*/
+
+COMMIT;
+
+----------------------------------------------------------------
+-- Write queries to answer the following questions:
+
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+
+-- What is the minimum and maximum weight of each type of animal?
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
+/* 1-- Create table animals */
+
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -9,5 +11,8 @@ CREATE TABLE animals (
     weight_kg FLOAT NOT NULL,
 );
 
+/* 2-- Update and delete table */
+
+/* - Add new column called species of type string */
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
     
